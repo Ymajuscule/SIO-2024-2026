a0539607f596bea802239a9736e19c0ce89b6ae5
Ymajuscule <j.saillot19@gmail.com>
2024-10-08 09:00:17 +0200

    Add files via upload

--- Julien/seance2/ReadTxt.py
+++ Julien/seance2/ReadTxt.py
@@ -1,1 +1,13 @@
+amis = {}
 
+with open('C:/Users/jsail/Desktop/Cours/python/Main.txt', 'r') as fichier:
+    for ligne in fichier:
+        prenom, age = ligne.strip().split(';')
+        if prenom != 'prenom':
+            amis[prenom] = age
+
+
+
+for i in amis:
+    print(f'{i} à {amis[i]}')
+print(amis)
