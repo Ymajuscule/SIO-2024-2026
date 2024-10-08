7ac4a9b8714194db9c8b0d3ff695c3e6d08da7d5
Ymajuscule <j.saillot19@gmail.com>
2024-10-08 08:58:37 +0200

    Delete Julien directory

--- Julien/FirstProjetc.py
+++
@@ -1,9 +0,0 @@
-#mon premier programme
-
-print('Mon 1er programme')
-
-ma_list = [0,1,2,'bleu',10e6,3.14,True,False]
-
-
-for elem in ma_list:
-    print("element=",elem,"de type:",type(elem),end=' ')
--- Julien/MesNotes.py
+++
@@ -1,9 +0,0 @@
-mes_notes = [15,8,12,11.5]
-
-def Main(list):
-    total = sum(list) / len(list)
-    print("la moyenne est de",total)
-    print("le nombre minimum", min(list))
-    print(max(list))
-
-Main(mes_notes)
